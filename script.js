4052874f8547db54cbf77d59bf2b6339f93e0d0c
Jdavidaa01 <jdavidaa.01@gmail.com>
2024-12-05 19:08:37 -0500

    END PALINDROME

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ let result = document.getElementById('result')
 //funcion ingreso
 let checkPalindromo = input =>{
     let originalInput=input;
-}
 
 //verificacion si es ta vacio el input y dan click
 if(input===''){
@@ -16,6 +15,30 @@ if(input===''){
 
 //borramos el span
 result.replaceChildren();
-
+// verificamos si es un palindromo 
 let lowerCaseStr = input.replace(/[^A-Za-z0-9]/gi, '').toLowerCase();
-let resultMsg= `${originalInput} ${lowerCaseStr === [...lowerCaseStr].reverse().join('')? 'is':'is not'} a palindrome.`;
+//primero al ingreso del input le decimos que deje todas las letras
+//y numeros del 0-9  luego trasformamos todo a minuscula
+let resultMsg= `${originalInput}${lowerCaseStr === [...lowerCaseStr].reverse().join('')? ' is':' is not'} a palindrome.`;
+//armamos el mensaje del span seria el texto ingresado
+
+let pTag= document.createElement('p');
+pTag.className='user-Input';
+pTag.innerText=resultMsg;
+result.appendChild(pTag);
+
+//mostramos el resultado
+result.classList.remove('hidden');
+};
+
+btn.addEventListener('click',() =>{
+    checkPalindromo(userInput.value);
+    userInput.value='';
+});
+
+userInput.addEventListener('keydown',e =>{
+    if(e.key === 'Enter'){
+        checkPalindromo(userInput.value);
+        userInput.value='';
+    }
+});
